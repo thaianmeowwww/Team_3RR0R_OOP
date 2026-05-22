2c36622b0395d85f24ff72efe192f550a8edaff9
Thái An <thaian@gmail.com>
2026-05-22 21:33:25 +0700

    Kiến trúc xong models

--- models/models.py
+++ models/models.py
@@ -207,4 +207,4 @@ engine = create_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def init_db():
-    Base.metadata.create_all(bind=engine)
+    Base.metadata.create_all(bind=engine)###
